89ab7ffaf4b789ec0bae6408562851f9e88dd6af
Ubuntu <ubuntu@ip-172-31-3-158.eu-west-2.compute.internal>
2019-03-19 15:01:45 +0000

    Adding a service file

---
+++ README.md
@@ -0,0 +1,3 @@
+To install dependencies on ubuntu 18.04:
+
+`sudo apt install build-essential python-dev libxml2 libxml2-dev zlib1g-dev`
---
+++ start_analysis.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd /home/ubuntu/vascular_TGFB1/analysis
+/home/ubuntu/.local/bin/jupyter notebook --ip 0.0.0.0 --config /home/ubuntu/cancer_heterogeneity/.jupyter/jupyter_notebook_config.py
